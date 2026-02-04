dde4ae047163c65e2568640b3c7b5f7cf2a94820
VamsiBoyana <vamsiboina1800@gmail.com>
2026-02-04 12:15:09 +0530

    modified server.js

--- TETRIS_BACKEND_LIVE/server.js
+++ TETRIS_BACKEND_LIVE/server.js
@@ -7,38 +7,38 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const GameHistory = require('./models/gameHistorySchema'); // adjust path as needed
 const cron = require('node-cron');
- 
+
 const app = express()
 dotenv.config()
 // app.use(cors());
 // app.use(cors(corsOptions));
 
 app.use(
-      cors({
-        allowedHeaders: "*",
-        exposedHeaders: ["token", "authorization"],
-        origin: [ 'https://stringtetris.com',
-  'https://stradmin.stringtetris.com',
-  'https://stringwithdrawadmin.stringgames.io',
-  'https://stringwithdraw.stringgames.io',
-],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-      }),
-    );
- 
+  cors({
+    allowedHeaders: "*",
+    exposedHeaders: ["token", "authorization"],
+    origin: ['https://stringtetris.com',
+      'https://stradmin.stringtetris.com',
+      'https://stringwithdrawadmin.stringgames.io',
+      'https://stringwithdraw.stringgames.io',
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  }),
+);
+
 connectToDb()
- 
+
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use("/backapi/api/auth", router);
- 
+
 const SESSION_TIMEOUT_MINUTES = 3; // set your timeout here
- 
+
 cron.schedule('* * * * *', async () => { // Runs every minute
   const timeoutDate = new Date(Date.now() - SESSION_TIMEOUT_MINUTES * 60 * 1000);
- 
+
   try {
     const result = await GameHistory.updateMany(
       {
@@ -54,9 +54,9 @@ cron.schedule('* * * * *', async () => { // Runs every minute
     console.error("Error expiring old pending games:", err);
   }
 });
- 
+
 // Start the server
 app.listen(process.env.PORT, () => {
-    console.log(`Server running successfully on ${process.env.PORT}`);
+  console.log(`Server running successfully on ${process.env.PORT}`);
 });
 
